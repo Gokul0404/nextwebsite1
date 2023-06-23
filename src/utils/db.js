82f99025ddfe729fdb929040ac388ddaa3be8e8f
Gokul0404 <gokul332020@gmail.com>
2023-06-23 17:45:14 +0530

    remove .env

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connect = async () => {
   // console.log(process.env.MONGOOSE_URL, "process.env.MONGOOSE_URL");
+  // mongodb+srv://gokul332020:sample@cluster0.aoyk9fm.mongodb.net/?retryWrites=true&w=majority
   try {
      await mongoose.connect(process.env.MONGOOSE_URL);
   } catch (error) {
